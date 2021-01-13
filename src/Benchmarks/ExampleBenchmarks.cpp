e9da8d1b1fad72b788703309b2899fc99eeb08b4
Aditya Sreekumar <aditya.sreekumar@gmail.com>
2021-01-12 21:50:54 -0500

    Run benchmark for protobuf tests

--- src/Benchmarks/ExampleBenchmarks.cpp
+++ src/Benchmarks/ExampleBenchmarks.cpp
@@ -1,6 +1,7 @@
 #include "Benchmarks/ExampleBenchmark.h"
 #include "Benchmarks/CommunicationData.h"
 #include "Benchmarks/CommunicationDataConcept.h"
+#include "Benchmarks/DataTypes/CommsProtocol.pb.h"
 
 #include <benchmark/benchmark.h>
 
@@ -36,9 +37,20 @@ static void ConceptBenchmark(benchmark::State &state)
   }
 }
 
+static void ProtobufBenchmark(benchmark::State &state)
+{
+  // Perform setup here
+  for (auto _ : state)
+  {
+    // This code gets benchmark::DoNotOptimize timed
+    benchmark::DoNotOptimize(NetworkMessage());
+  }
+}
+
 // Register the function as a benchmark
 BENCHMARK(SampleBenchmark);
 BENCHMARK(ConceptBenchmark);
+BENCHMARK(ProtobufBenchmark);
 
 BENCHMARK_TEMPLATE(SampleVectorBenchmark, CommunicationData)->RangeMultiplier(2)->Range(1 << 10, 1 << 18)->Complexity(benchmark::oN);
 BENCHMARK_TEMPLATE(SampleVectorBenchmark, int)->RangeMultiplier(2)->Range(1 << 10, 1 << 18)->Complexity(benchmark::oN);
