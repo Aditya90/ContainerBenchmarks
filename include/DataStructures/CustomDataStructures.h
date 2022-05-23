f51c14ecdb521303fa0d1e16acf7f46ab98bf83b
Aditya Sreekumar <aditya.sreekumar@gmail.com>
2022-05-22 20:59:35 -0400

    Add test to ensure multiple write threads works

--- include/DataStructures/CustomDataStructures.h
+++ include/DataStructures/CustomDataStructures.h
@@ -15,6 +15,7 @@ A.push_back(5) // -, (2), 3, 4, -, -
 */
 #include <memory>
 #include <iostream>
+#include <shared_mutex>
 
 template <typename T>
 class Queue
@@ -35,13 +36,46 @@ public:
         capacity_ = capacity;
         queueData_ = std::make_unique<T[]>(capacity_);
     }
+    /**
+     * @brief Push a value to the front of the queue
+     * @param val Value to push into the queue
+     */
     void push_back(const T &val);
+
+    /**
+     * @brief Get the value from the front of the queue and pop it
+     *
+     * @return T Value at the front of the queue
+     */
     T pop_front();
 
+    /**
+     * @brief Print information about the queue
+     *
+     */
     void print();
 
-    size_t getSize() { return size_; }
-    size_t getCapacity() { return capacity_; }
+    /**
+     * @brief Get the Size object
+     *
+     * @return size_t Size of the queue
+     */
+    size_t getSize()
+    {
+        std::shared_lock lock(queueDataMutex_);
+        return size_;
+    }
+
+    /**
+     * @brief Get the Capacity object
+     *
+     * @return size_t Capacity of the queue
+     */
+    size_t getCapacity()
+    {
+        std::shared_lock lock(queueDataMutex_);
+        return capacity_;
+    }
 
 private:
     std::unique_ptr<T[]> queueData_;
@@ -49,22 +83,37 @@ private:
     size_t size_{0};
     size_t capacity_{10};
 
+    std::shared_mutex queueDataMutex_;
+
+    /**
+     * @brief Resize the queue to twice its existing capacity
+     */
     void resizeQueue()
     {
         std::unique_ptr<T[]> newQueueData(new T[capacity_ * 2]);
-        std::copy(queueData_.get() + front_, queueData_.get() + capacity_, newQueueData.get() + front_);
-        std::copy(queueData_.get(), queueData_.get() + front_, newQueueData.get() + capacity_);
+
+        std::move(queueData_.get() + front_, queueData_.get() + capacity_, newQueueData.get() + front_);
+        std::move(queueData_.get(), queueData_.get() + front_, newQueueData.get() + capacity_);
 
         queueData_.swap(newQueueData);
         capacity_ = capacity_ * 2;
     }
 
+    /**
+     * @brief Find the next index in the queue data structure to fill a value into
+     *
+     * @return size_t Index location to fill
+     */
     size_t findNextIndexToFill()
     {
         size_t nextIndex = (front_ + size_) % capacity_;
         return nextIndex;
     }
 
+    /**
+     * @brief Increment the index pointing to the front of the queue
+     *
+     */
     void incrementFront()
     {
         front_ = (front_ + 1) % capacity_;
@@ -74,6 +123,7 @@ private:
 template <typename T>
 void Queue<T>::push_back(const T &val)
 {
+    std::unique_lock lock(queueDataMutex_);
     if (size_ == capacity_)
     {
         resizeQueue();
@@ -88,6 +138,7 @@ void Queue<T>::push_back(const T &val)
 template <typename T>
 T Queue<T>::pop_front()
 {
+    std::unique_lock lock(queueDataMutex_);
     if (size_ == 0)
     {
         throw std::runtime_error("Empty queue");
@@ -104,6 +155,8 @@ T Queue<T>::pop_front()
 template <typename T>
 void Queue<T>::print()
 {
+    std::shared_lock lock(queueDataMutex_);
+
     std::cout << "Data: ";
     for (size_t index = 0; index < capacity_; index++)
     {
