6afa8e3f57f8f762030defdab78ec5a1600c3035
Aditya Sreekumar <aditya.sreekumar@gmail.com>
2022-05-22 21:42:12 -0400

    Add queue data structure to the existing benchmark

--- src/ExampleBenchmarks.cpp
+++ src/ExampleBenchmarks.cpp
@@ -1,5 +1,6 @@
 #include "ExampleBenchmark.h"
 #include "CommunicationData.h"
+#include "DataStructures/CustomDataStructures.h"
 
 #include <benchmark/benchmark.h>
 
@@ -25,8 +26,22 @@ static void SampleVectorBenchmark(benchmark::State &state)
   state.SetComplexityN(state.range(0));
 }
 
+template <class TypeToTest>
+static void QueueBenchmark(benchmark::State &state)
+{
+  // Perform setup here
+  for (auto _ : state)
+  {
+    // This code gets benchmark::DoNotOptimize timed
+    benchmark::DoNotOptimize(Queue<TypeToTest>{state.range(0)});
+  }
+  state.SetComplexityN(state.range(0));
+}
+
 // Register the function as a benchmark
 BENCHMARK(SampleBenchmark);
 
 BENCHMARK_TEMPLATE(SampleVectorBenchmark, CommunicationData)->RangeMultiplier(2)->Range(1 << 10, 1 << 18)->Complexity(benchmark::oN);
 BENCHMARK_TEMPLATE(SampleVectorBenchmark, int)->RangeMultiplier(2)->Range(1 << 10, 1 << 18)->Complexity(benchmark::oN);
+BENCHMARK_TEMPLATE(QueueBenchmark, CommunicationData)->RangeMultiplier(2)->Range(1 << 10, 1 << 18)->Complexity(benchmark::oN);
+BENCHMARK_TEMPLATE(QueueBenchmark, int)->RangeMultiplier(2)->Range(1 << 10, 1 << 18)->Complexity(benchmark::oN);
